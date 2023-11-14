7937b7d574f4165e767c9849e29de391f657b7be
eureka-cpu <github.eureka@gmail.com>
2023-11-14 00:42:56 -0600

    add flake description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,10 @@
 {
-  description = "A very basic flake";
+  description =
+    "
+      [my•nix•ui], derived from the japanese word 醜い(minikui), meaning ugly,
+      or unsightly, is a series of widgets for Linux, written with eww & yuck,
+      which together provide a full desktop environment, loaded via nix flake.
+    ";
 
   outputs = { self, nixpkgs }: {
 
